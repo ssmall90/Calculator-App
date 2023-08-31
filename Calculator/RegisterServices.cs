4dc7db0d2fc720a10fe29d16791be2778b40d62c
ssmall90 <ssmall9595@icloud.com>
2023-08-31 11:39:12 +0100

    Add project files.

---
+++ Calculator/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Calculator
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+        }
+    }
+}
